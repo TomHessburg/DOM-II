8f4a9969c364a1fc1b7b49ac93695b123b1da98e
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-29 16:26:47 -0500

    MVP done, pretty ugly tho, gunu move on to stretch and see if i can make it not look absolutely horendous hahaha

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@
 let navBar = document.querySelector(".main-navigation");
 let upperImg = document.querySelector(".intro img");
 let secondImg = document.querySelector(".img-content img");
-
+let headingTxt = document.querySelector(".intro h2")
+let bottomButton = document.querySelector(".btn");
+let pickDest = document.querySelector(".content-destination h2");
+let topParagraph = document.querySelector(".intro p");
 
 navBar.addEventListener("mouseover", function(){
     navBar.style.backgroundColor = "black";
@@ -25,9 +28,36 @@ window.addEventListener("scroll",() => {//scrolls screen down 7px per 350ms up t
     }
 })
 
-secondImg.addEventListener("click", () => {
-    secondImg.style.cursor = "pointer";
-    if(scrollY > 400){
+secondImg.style.cursor = "pointer";
+secondImg.addEventListener("click", () => {//click on second pic 
+    
         window.scroll(0, scrollY += 200);
-    }
+  
+})
+
+
+window.addEventListener("keydown",function(e){
+    e = e.code.split("")
+    headingTxt.textContent = `you clicked ${e[e.length-1]}`;//changed header text to what you click
+})
+
+window.addEventListener("keyup",function(e){
+    e = e.code.split("")
+    headingTxt.textContent = `you released ${e[e.length-1]}`;//changed aboves result 
+})
+
+
+bottomButton.addEventListener("dblclick", function(){//bottom left button goes black when dbl clicked
+    bottomButton.style.backgroundColor = "black";
+})
+
+window.addEventListener("wheel", (e) => {//when wheel is move, text gets bigger and turns red...
+    pickDest.style.fontSize = "50px";
+    pickDest.style.color = "red";
+})
+
+
+
+topParagraph.addEventListener("mousemove", function(e){
+    topParagraph.style.marginLeft = "50px";
 })
